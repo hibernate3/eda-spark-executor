a57fd24da28e3e7175bfb5721e888a1acf4d101e
wangyuhang <wangyuhang@evergrande.com>
2019-12-26 10:58:08 +0800

    注释代码去除

--- src/main/scala/com/eda/spark/sql/smartvillage/HiveExecutor.scala
+++ src/main/scala/com/eda/spark/sql/smartvillage/HiveExecutor.scala
@@ -62,22 +62,6 @@ object HiveExecutor extends Serializable {
     writeToMysql("cbox_smartvillage_v0.1", "ic_card_status_result", resultDf, mode)
   }
 
-  //  def executeICCard(sparkSession: SparkSession): Unit = {
-  //    val sql = "SELECT rowkey, cardtype, owneruuid, ownertypetext, cardstatustext FROM community.community_idcarddetail " +
-  //      "WHERE cardtype!='' AND owneruuid!='' AND ownertypetext !='' AND cardstatustext !=''"
-  //    val df = sparkSession.sql(sql)
-  //    df.cache().createOrReplaceTempView("ic_card")
-  //    df.show()
-  //
-  //    //IC持卡人分布
-  //    var resultDf = sparkSession.sql("SELECT cardtype, ownertypetext, COUNT(DISTINCT rowkey) as countNum FROM ic_card GROUP BY cardtype, ownertypetext")
-  //    writeToMysql("cbox_smartvillage_v0.1", "ic_card_user_result", resultDf)
-  //
-  //    //IC状态分布
-  //    resultDf = sparkSession.sql("SELECT cardtype, cardstatustext,COUNT(DISTINCT rowkey) as countNum FROM ic_card GROUP BY cardtype, cardstatustext")
-  //    writeToMysql("cbox_smartvillage_v0.1", "ic_card_status_result", resultDf)
-  //  }
-
   def executeCarParking(sparkSession: SparkSession, mode: Int): Unit = {
     var sql: String = ""
     if (mode == 0) {
@@ -211,96 +195,6 @@ object HiveExecutor extends Serializable {
     writeToMysql("cbox_smartvillage_v0.1", "car_parking_energy_result", resultDf, mode)
   }
 
-  //  def executeCarParking(sparkSession: SparkSession): Unit = {
-  //    var sql = "SELECT rowkey, stopTime, intime, outtime, entermodetext, exitmodetext, carporttypetext, consumemoney, payedmoney " +
-  //      "FROM community.community_car " +
-  //      "WHERE carporttypetext!='' AND exitmodetext!='' AND entermodetext!='' AND stoptime!='' AND intime!='' AND outtime!='' AND payedmoney!=''"
-  //    val df = sparkSession.sql(sql)
-  //    df.cache().createOrReplaceTempView("car_parking")
-  //    //    df.show()
-  //
-  //    sparkSession.udf.register("stopTimeLevel", (str: String) => {
-  //      try {
-  //        if (str.contains("天")) {
-  //          "24小时以上"
-  //        } else if (str.contains("小时")) {
-  //          val hour: Int = str.split("小时")(0).toInt
-  //          if (hour == 1) {
-  //            "1-2小时"
-  //          } else if (hour >= 2 && hour < 4) {
-  //            "2-4小时"
-  //          } else if (hour >= 4 && hour < 12) {
-  //            "4-12小时"
-  //          } else if (hour >= 12 && hour < 24) {
-  //            "12-24小时"
-  //          } else {
-  //            "其他"
-  //          }
-  //        } else {
-  //          "1小时内"
-  //        }
-  //      } catch {
-  //        case e: Exception => "其他"
-  //      }
-  //    })
-  //
-  //    sparkSession.udf.register("intimeLevel", (str: String) => {
-  //      try {
-  //        val hour: Int = str.split(" ")(1).split(":")(0).toInt
-  //        if (hour >= 0 && hour < 8) {
-  //          "0-8点"
-  //        } else if (hour >= 8 && hour < 10) {
-  //          "8-10点"
-  //        } else if (hour >= 10 && hour < 12) {
-  //          "10-12点"
-  //        } else if (hour >= 12 && hour < 14) {
-  //          "12-14点"
-  //        } else if (hour >= 14 && hour < 18) {
-  //          "14-18点"
-  //        } else if (hour >= 18 && hour < 22) {
-  //          "18-22点"
-  //        } else if (hour >= 22 && hour < 24) {
-  //          "22-34点"
-  //        } else {
-  //          "其他"
-  //        }
-  //      } catch {
-  //        case e: Exception => "其他"
-  //      }
-  //    })
-  //
-  //    sparkSession.udf.register("timeDay", (str: String) => {
-  //      try {
-  //        str.split(" ")(0)
-  //      } catch {
-  //        case e: Exception => "其他"
-  //      }
-  //    })
-  //
-  //    val tempResult = sparkSession.sql("SELECT rowkey, stopTimeLevel(stopTime) stopTimeLevel, intimeLevel(intime) intimeLevel, timeDay(outtime) outtime, entermodetext, exitmodetext, carporttypetext, consumemoney, payedmoney FROM car_parking")
-  //    tempResult.createOrReplaceTempView("car_parking")
-  //    tempResult.show()
-  //
-  //    //机动车停车时长分布
-  //    var resultDf = sparkSession.sql("SELECT stopTimeLevel, COUNT(DISTINCT rowkey) as countNum FROM car_parking GROUP BY stopTimeLevel")
-  //    writeToMysql("cbox_smartvillage_v0.1", "car_parking_stoptime_result", resultDf)
-  //
-  //    //机动车入场时间分布
-  //    resultDf = sparkSession.sql("SELECT intimeLevel, COUNT(DISTINCT rowkey) as countNum FROM car_parking GROUP BY intimeLevel")
-  //    writeToMysql("cbox_smartvillage_v0.1", "car_parking_intime_result", resultDf)
-  //
-  //    //机动车出场开闸方式
-  //    resultDf = sparkSession.sql("SELECT exitmodetext, COUNT(DISTINCT rowkey) as countNum FROM car_parking GROUP BY exitmodetext")
-  //    writeToMysql("cbox_smartvillage_v0.1", "car_parking_exitmode_result", resultDf)
-  //
-  //    //车辆类型比例
-  //    resultDf = sparkSession.sql("SELECT carporttypetext, COUNT(DISTINCT rowkey) as countNum FROM car_parking GROUP BY carporttypetext")
-  //    writeToMysql("cbox_smartvillage_v0.1", "car_parking_parkingtype_result", resultDf)
-  //
-  //    //车辆缴费
-  //    resultDf = sparkSession.sql("SELECT outtime, SUM(payedmoney) as payedmoney_total, SUM(consumemoney) as consumemoney_total FROM car_parking GROUP BY outtime")
-  //    writeToMysql("cbox_smartvillage_v0.1", "car_parking_pay_result", resultDf)
-  //  }
 
   def executeDeviceAuth(sparkSession: SparkSession, mode: Int): Unit = {
     var sql: String = ""
@@ -396,78 +290,6 @@ object HiveExecutor extends Serializable {
     writeToMysql("cbox_smartvillage_v0.1", "device_auth_credencetype_result", resultDf, mode)
   }
 
-  //  def executeDeviceAuth(sparkSession: SparkSession): Unit = {
-  //    val sql = "SELECT rowkey, usertype, devicename, subdevicename, credencetype FROM community.community_deviceauthdetail WHERE usertype!='' AND devicename!='' AND subdevicename!='' AND credencetype!=''"
-  //    val df = sparkSession.sql(sql)
-  //    df.cache().createOrReplaceTempView("device_auth")
-  //    //    df.show()
-  //
-  //    sparkSession.udf.register("userTypeConverter", (value: Int) => {
-  //      try {
-  //        if (value == 1) {
-  //          "业主"
-  //        } else if (value == 2) {
-  //          "家属"
-  //        } else if (value == 3) {
-  //          "租客"
-  //        } else if (value == 4) {
-  //          "访客"
-  //        } else if (value == 5) {
-  //          "物业"
-  //        } else if (value == 6) {
-  //          "快递"
-  //        } else if (value == 7) {
-  //          "外卖"
-  //        } else {
-  //          "其他"
-  //        }
-  //      } catch {
-  //        case e: Exception => "其他"
-  //      }
-  //    })
-  //
-  //    sparkSession.udf.register("credenceTypeConverter", (value: Int) => {
-  //      try {
-  //        if (value == 5) {
-  //          "车牌"
-  //        } else if (value == 2 || value == 3) {
-  //          "卡片"
-  //        } else if (value == 7) {
-  //          "人脸"
-  //        } else if (value == 6) {
-  //          "指纹"
-  //        } else if (value == 8) {
-  //          "二维码"
-  //        } else if (value == 9) {
-  //          "蓝牙"
-  //        } else if (value == 12) {
-  //          "NFC"
-  //        } else if (value == 10) {
-  //          "密码"
-  //        } else {
-  //          "其他"
-  //        }
-  //      } catch {
-  //        case e: Exception => "其他"
-  //      }
-  //    })
-  //
-  //    val tempResult = sparkSession.sql("SELECT rowkey, userTypeConverter(usertype) usertype, devicename, subdevicename, credenceTypeConverter(credencetype) credencetype FROM device_auth")
-  //    tempResult.createOrReplaceTempView("device_auth")
-  //    tempResult.show()
-  //
-  //    //人员类型比例
-  //    var resultDf = sparkSession.sql("SELECT usertype, credencetype, COUNT(DISTINCT rowkey) as countNum FROM device_auth GROUP BY usertype, credencetype")
-  //    writeToMysql("cbox_smartvillage_v0.1", "device_auth_usertype_result", resultDf)
-  //
-  //    //设备类型比例
-  //    resultDf = sparkSession.sql("SELECT devicename, COUNT(DISTINCT rowkey) as countNum FROM device_auth GROUP BY devicename")
-  //    writeToMysql("cbox_smartvillage_v0.1", "device_auth_devicetype_result", resultDf)
-  //
-  //    //凭证类型比例
-  //    resultDf = sparkSession.sql("SELECT credencetype, COUNT(DISTINCT rowkey) as countNum FROM device_auth GROUP BY credencetype")
-  //    writeToMysql("cbox_smartvillage_v0.1", "device_auth_credencetype_result", resultDf)
-  //  }
 
   def executeDeviceAccess(sparkSession: SparkSession, mode: Int): Unit = {
     var sql: String = ""
@@ -577,90 +399,6 @@ object HiveExecutor extends Serializable {
     writeToMysql("cbox_smartvillage_v0.1", "device_access_updatetime_result", resultDf, mode)
   }
 
-  //  def executeDeviceAccess(sparkSession: SparkSession): Unit = {
-  //    val sql = "SELECT rowkey, userid, usertype, devicename, updatetime, description from community.community_deviceacessdetail WHERE userid!='' AND usertype!='' AND devicename!='' AND updatetime!='' AND description!=''"
-  //    val df = sparkSession.sql(sql)
-  //    df.cache().createOrReplaceTempView("device_access")
-  //    //    df.show()
-  //
-  //    sparkSession.udf.register("userTypeConverter", (value: Int) => {
-  //      try {
-  //        if (value == 1) {
-  //          "业主"
-  //        } else if (value == 2) {
-  //          "家属"
-  //        } else if (value == 3) {
-  //          "租客"
-  //        } else if (value == 4) {
-  //          "访客"
-  //        } else if (value == 5) {
-  //          "物业"
-  //        } else if (value == 6) {
-  //          "快递"
-  //        } else if (value == 7) {
-  //          "外卖"
-  //        } else {
-  //          "其他"
-  //        }
-  //      } catch {
-  //        case e: Exception => "其他"
-  //      }
-  //    })
-  //
-  //    sparkSession.udf.register("updatetimeLevel", (str: String) => {
-  //      try {
-  //        val hour: Int = str.split(" ")(1).split(":")(0).toInt
-  //        if (hour >= 0 && hour < 8) {
-  //          "0-8点"
-  //        } else if (hour >= 8 && hour < 10) {
-  //          "8-10点"
-  //        } else if (hour >= 10 && hour < 12) {
-  //          "10-12点"
-  //        } else if (hour >= 12 && hour < 14) {
-  //          "12-14点"
-  //        } else if (hour >= 14 && hour < 18) {
-  //          "14-18点"
-  //        } else if (hour >= 18 && hour < 22) {
-  //          "18-22点"
-  //        } else if (hour >= 22 && hour < 24) {
-  //          "22-34点"
-  //        } else {
-  //          "其他"
-  //        }
-  //      } catch {
-  //        case e: Exception => "其他"
-  //      }
-  //    })
-  //
-  //    sparkSession.udf.register("timeDay", (str: String) => {
-  //      try {
-  //        str.split(" ")(0)
-  //      } catch {
-  //        case e: Exception => "其他"
-  //      }
-  //    })
-  //
-  //    val tempResult = sparkSession.sql("SELECT rowkey, userid, userTypeConverter(usertype) usertype, devicename, updatetimeLevel(updatetime) updatetimeLevel, timeDay(updatetime) updatetimeDay, description from device_access")
-  //    tempResult.createOrReplaceTempView("device_access")
-  //    tempResult.show()
-  //
-  //
-  //    //人员类型趋势
-  //    var resultDf = sparkSession.sql("SELECT usertype, updatetimeDay, COUNT(DISTINCT rowkey) as countNum FROM device_access GROUP BY usertype, updatetimeDay")
-  //    writeToMysql("cbox_smartvillage_v0.1", "device_access_usertype_result", resultDf)
-  //
-  //    //设备类型趋势
-  //    resultDf = sparkSession.sql("SELECT devicename, updatetimeDay, COUNT(DISTINCT rowkey) as countNum FROM device_access GROUP BY devicename, updatetimeDay")
-  //    writeToMysql("cbox_smartvillage_v0.1", "device_access_devicetype_result", resultDf)
-  //
-  //    //凭证类型趋势
-  //    resultDf = sparkSession.sql("SELECT description, updatetimeDay, COUNT(DISTINCT rowkey) as countNum FROM device_access GROUP BY description, updatetimeDay")
-  //    writeToMysql("cbox_smartvillage_v0.1", "device_access_description_result", resultDf)
-  //
-  //    //设备通行时间分布
-  //    resultDf = sparkSession.sql("SELECT devicename, updatetimeDay, COUNT(DISTINCT rowkey) as countNum FROM device_access GROUP BY devicename, updatetimeDay")
-  //    writeToMysql("cbox_smartvillage_v0.1", "device_access_updatetime_result", resultDf)
-  //  }
 
   def executePayment(sparkSession: SparkSession, mode: Int): Unit = {
     sparkSession.udf.register("payment_type_name", (value: Int) => {
@@ -731,50 +469,6 @@ object HiveExecutor extends Serializable {
     resultDf = sparkSession.sql("SELECT pay_date, court_uuid, payment_type_name(payment_type) as payment_type, total_money from payment_money")
     writeToMysql("cbox_smartvillage_v0.1", "payment_money_result", resultDf, mode)
   }
-
-  //  def executePayment(sparkSession: SparkSession): Unit = {
-  //    val url = "jdbc:postgresql://10.101.70.169:5432/hdsc_db"
-  //    val prop = new Properties()
-  //    prop.put("user", "hdsc_postgres")
-  //    prop.put("password", "hdsc_postgres")
-  //    prop.put("driver", "org.postgresql.Driver")
-  //
-  //    sparkSession.udf.register("payment_type_name", (value: Int) => {
-  //      try {
-  //        if (value == 1) {
-  //          "月保停车"
-  //        } else if (value == 2) {
-  //          "固定车位"
-  //        } else if (value == 3) {
-  //          "物业缴费"
-  //        } else if (value == 4 || value == 6) {
-  //          "临时停车"
-  //        } else if (value == 5) {
-  //          "预缴"
-  //        } else {
-  //          "其他"
-  //        }
-  //      } catch {
-  //        case e: Exception => "其他"
-  //      }
-  //    })
-  //
-  //    var sql = "SELECT left(cast(r.pay_time as VARCHAR), 10) as pay_date, o.payment_type, count(*) as total_times FROM fm.fm_payment_record as r INNER JOIN fm.fm_payment_order as o on o.out_trade_no = r.out_trade_no GROUP BY pay_date, o.payment_type"
-  //    var df = sparkSession.read.jdbc(url, s"(${sql}) t", prop)
-  //    df.cache().createOrReplaceTempView("payment_times")
-  //
-  //    //业主App缴费笔数
-  //    var resultDf = sparkSession.sql("SELECT pay_date, payment_type_name(payment_type) as payment_type, total_times from payment_times")
-  //    writeToMysql("cbox_smartvillage_v0.1", "payment_times_result", resultDf)
-  //
-  //    sql = "SELECT left(cast(r.pay_time as VARCHAR), 10) as pay_date, o.payment_type, cast(SUM(cast(total_amount as FLOAT8)) AS decimal(10,2)) as total_money FROM fm.fm_payment_record as r INNER JOIN fm.fm_payment_order as o on o.out_trade_no = r.out_trade_no GROUP BY pay_date, o.payment_type"
-  //    df = sparkSession.read.jdbc(url, s"(${sql}) t", prop)
-  //    df.cache().createOrReplaceTempView("payment_money")
-  //
-  //    //业主App缴费金额
-  //    resultDf = sparkSession.sql("SELECT pay_date, payment_type_name(payment_type) as payment_type, total_money from payment_money")
-  //    writeToMysql("cbox_smartvillage_v0.1", "payment_money_result", resultDf)
-  //  }
 
   def executeBaseCourt(sparkSession: SparkSession, mode: Int): Unit = {
     var sql: String = ""
